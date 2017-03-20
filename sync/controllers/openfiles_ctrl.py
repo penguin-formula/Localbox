98105e128c7a867f7ef33367ac3589b75b843e49
Wilson Santos <wilson.santos@penguinformula.com>
2017-03-20 14:20:27 +0000

    remove file from opened files. verify if the file still exists when loading the opened files

--- sync/controllers/openfiles_ctrl.py
+++ sync/controllers/openfiles_ctrl.py
@@ -1,6 +1,9 @@
 import pickle
 from logging import getLogger
 
+from os.path import exists
+
+from loxcommon import os_utils
 from sync.defaults import LOCALBOX_OPENFILES
 
 
@@ -14,6 +17,16 @@ def add(filename):
     save(openfiles_list)
 
 
+def remove(filesystem_path):
+    openfiles_list = load()
+    if filesystem_path in openfiles_list:
+        os_utils.shred(filesystem_path)
+        openfiles_list.remove(filesystem_path)
+        save(openfiles_list)
+    else:
+        getLogger(__name__).error('%s was not found in the list of opened files' % filesystem_path)
+
+
 def save(openfiles_list):
     pickle.dump(openfiles_list, open(LOCALBOX_OPENFILES, 'wb'))
     getLogger(__name__).debug('saved opened files: %s' % openfiles_list)
@@ -22,9 +35,14 @@ def save(openfiles_list):
 def load():
     try:
         with open(LOCALBOX_OPENFILES, 'rb') as f:
-            openfiles_list = pickle.load(f)
-            getLogger(__name__).debug('found this opened files: %s' % openfiles_list)
-            return openfiles_list
+            old_openfiles_list = pickle.load(f)
+            openfiles_list = list()
+            getLogger(__name__).debug('found this opened files: %s' % old_openfiles_list)
+            for opened_file in old_openfiles_list:
+                if exists(opened_file):
+                    openfiles_list.append(opened_file)
     except IOError:
         openfiles_list = list()
-        save(openfiles_list)
+
+    save(openfiles_list)
+    return openfiles_list
