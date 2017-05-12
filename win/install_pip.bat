e3d9bc65b1e0bce3461aa1ac1693f1ec353722ea
Pedro Melgueira <pedromelgueira@gmail.com>
2017-05-12 10:20:18 +0100

    Fixes version of wx

--- win/install_pip.bat
+++ win/install_pip.bat
@@ -4,4 +4,4 @@ echo Installing dependencies...
 CD %install_dir% 
 python.exe get-pip.py
 python.exe -m pip install psutil python-gnupg==0.3.8 pycrypto watchdog==0.8.3 pyzmq==16.0.2
-python -m pip install -U --pre -f https://wxpython.org/Phoenix/snapshot-builds/ wxPython_Phoenix
+python -m pip install -U --pre -f https://wxpython.org/Phoenix/snapshot-builds/ wxPython
