e51fb2fa8f405edfe4eb25e133c4d52af6d5ea55
Pedro Melgueira <pedromelgueira@gmail.com>
2017-05-12 11:45:42 +0100

    Adds inter process communication for poxis systems

--- sync/notif/notifs_util.py
+++ sync/notif/notifs_util.py
@@ -2,17 +2,28 @@
 This file declares some utilities for the communication part of the client.
 """
 
+
+import os
+
+
 zmq_file_op_notif = "FILE_OP_NOTIF"
 zmq_gui_notif = "GUI_NOTIF"
 zmq_gui_heartbeat = "GUI_HEARTBEAT"
 zmq_heartbeat_req = "HEARTBEAT_REQ"
 
-# TODO: Get free port from range
-zmq_ipc_push_bind = "tcp://*:9000"
-zmq_ipc_pub_bind  = "tcp://*:9001"
+if os.name == "posix":
+    zmq_ipc_push_bind = "ipc://push"
+    zmq_ipc_pub_bind  = "ipc://pub"
 
-zmq_ipc_push = "tcp://localhost:9000"
-zmq_ipc_pub  = "tcp://localhost:9001"
+    zmq_ipc_push = "ipc://push"
+    zmq_ipc_pub  = "ipc://pub"
+
+else:
+    zmq_ipc_push_bind = "tcp://*:9000"
+    zmq_ipc_pub_bind  = "tcp://*:9001"
+
+    zmq_ipc_push = "tcp://localhost:9000"
+    zmq_ipc_pub  = "tcp://localhost:9001"
 
 
 def mogrify(opt, payload):
