5dc56bdb2ca8aba31cc8e97b1b92b1b2da4b7f21
Micael Grilo <micael.grilo@penguinformula.com>
2019-07-18 12:21:29 +0100

    Implement in memory process mount with fuse

--- sync/memory_fs.py
+++ sync/memory_fs.py
@@ -3,6 +3,55 @@ import os, sys
 import random, string, time
 from fs.memoryfs import MemoryFS
 from fs.expose import fuse
+from multiprocessing import Process
+
+from six import b
+NATIVE_ENCODING = sys.getfilesystemencoding()
+
+
+class LocalBoxMountProcess(fuse.MountProcess):
+    '''
+    Class that will mount virtual folder
+
+    '''
+    def __init__(self, fs, path, fuse_opts={}, nowait=False, **kwds):
+        # super(LocalBoxMountProcess, self).__init__(fs, path, fuse_opts, nowait, **kwds)
+        self.path = path
+
+        (r, w) = os.pipe()
+
+        data = (fs, path, fuse_opts, r, w)
+        p = Process(target=fuse.MountProcess._do_mount_wait, args=(data,))
+        p.start()
+
+        os.close(w)
+        byte = os.read(r, 1)
+        if byte != b("S"):
+            err_text = os.read(r, 20)
+            self.terminate()
+            if hasattr(err_text, 'decode'):
+                err_text = err_text.decode(NATIVE_ENCODING)
+            raise RuntimeError("FUSE error: " + err_text)
+
+
+def mount(fs, path, foreground=False, ready_callback=None, unmount_callback=None, **kwds):
+    """Mount the given FS at the given path, using FUSE.
+
+    By default, this function spawns a new background process to manage the
+    FUSE event loop.
+    """
+    path = os.path.expanduser(path)
+    mp = LocalBoxMountProcess(fs, path, kwds)
+    if ready_callback:
+        ready_callback()
+    if unmount_callback:
+        orig_unmount = mp.unmount
+
+        def new_unmount():
+            orig_unmount()
+            unmount_callback()
+        mp.unmount = new_unmount
+    return mp
 
 
 class LocalBoxMemoryFS():
@@ -31,11 +80,7 @@ class LocalBoxMemoryFS():
         else:
             self.mount_directory = os.path.join(os.path.expanduser('~')) +'/mtdecoded/'
 
-            if not os.path.exists(self.mount_directory):
-                os.makedirs(self.mount_directory)
-                fuse.mount(self.memory, self.mount_directory)
-    
-    
+
     def createfile(self, path, content, wipe=True):
         self.memory.createfile(path, wipe=wipe)
         with self.memory.open(path, "wb") as f:
@@ -43,14 +88,18 @@ class LocalBoxMemoryFS():
         
         if not os.path.exists(self.mount_directory):
             os.makedirs(self.mount_directory)
-            fuse.mount(self.memory, self.mount_directory)
-        else:
-            fuse.unmount(self.mount_directory)
-            fuse.mount(self.memory, self.mount_directory)
-        
+            mount(self.memory, self.mount_directory)
+        # else:
+        #     fuse.unmount(self.mount_directory)
+        #     mount(self.memory, self.mount_directory)
+        # If system is mounted with video it can't be unmounted, find a wait o update mounted resources TODO
+
         return self.mount_directory + path
 
     def destroy(self):
-        time.sleep(2) #Wait 2 seconds while app open's file then destroy and unmount
-        fuse.unmount(self.mount_directory)
-        os.removedirs(self.mount_directory)
+        time.sleep(2)
+        try:
+            fuse.unmount(self.mount_directory)
+            os.removedirs(self.mount_directory)
+        except OSError: #Mounted in use, try again
+            self.destroy()
